29c18fffc30811b4523189f60fe3d50e2df3b173
Ramy Hadid <ramyhadid2005.rh@gmail.com>
2023-11-11 12:30:41 +0100

    Verification Process

--- svelte.config.js
+++ svelte.config.js
@@ -1,42 +1,62 @@
 import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess"
+import { importAssets } from 'svelte-preprocess-import-assets'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: "demo",
+			assets: "demo",
+			fallback: "bundle.html",
+			precompress: false,
+			strict: true
+		}),
+		appDir: "build"
+	},
 
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				// // ignore deliberate link to shiny 404 page
-				// if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
-				// 	return;
-				// }
+	preprocess: [
+		preprocess(
+			[importAssets()]
+		)
+	],
 
-				// // otherwise fail the build
-				// throw new Error(message);
+	prerender: {
+		handleHttpError: ({ path, referrer, message }) => {
+			// // ignore deliberate link to shiny 404 page
+			// if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+			// 	return;
+			// }
 
-				// ? This is the code part
+			// // otherwise fail the build
+			// throw new Error(message);
 
-				console.error(`HTTP error: ${message} (${path})`);
+			// ? This is the code part
 
-				// If the error is a 404, redirect to the custom 404 page.
-				if (message.includes('404')) {
-					return {
-						statusCode: 404,
-						headers: {
-							Location: '/not-found',
-						},
-					};
-				}
+			console.error(`HTTP error: ${message} (${path})`);
 
-				// Otherwise, fail the build.
-				throw new Error(message)
+			// If the error is a 404, redirect to the custom 404 page.
+			if (message.includes('404')) {
+				return {
+					statusCode: 404,
+					headers: {
+						Location: '/not-found',
+					},
+				};
 			}
+
+			// Otherwise, fail the build.
+			throw new Error(message)
 		}
-	}
+	},
+
+	vite: {
+		optimizeDeps: {
+			include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
+		}
+	},
+
 };
 
 export default config;
