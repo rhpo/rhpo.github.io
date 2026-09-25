730837118e85e499da79aae4f1831f158903daf9
Ramy Hadid <ramyhadid2005.rh@gmail.com>
2026-09-25 01:40:52 +0100

    Fix QC Flag not appearing

--- src/routes/(portfolio)/+layout.svelte
+++ src/routes/(portfolio)/+layout.svelte
@@ -1,18 +1,25 @@
 <script>
 	import Navbar from '$lib/components/portfolio/Navbar.svelte';
+	import LanguageToggle from '$lib/components/portfolio/LanguageToggle.svelte';
 	import '../../app.css';
 	import { ModeWatcher, setMode } from 'mode-watcher';
 	import { locale } from '$lib/i18n';
 	import { onMount } from 'svelte';
+	import { afterNavigate } from '$app/navigation';
 
 	setMode('dark');
 
 	onMount(() => {
 		locale.init();
 	});
+
+	afterNavigate(() => {
+		locale.init();
+	});
 </script>
 
 <ModeWatcher />
+<LanguageToggle />
 <div class="relative mx-auto min-h-screen max-w-2xl bg-background px-6 py-12 font-sans antialiased sm:py-24">
 	<slot></slot>
 	<Navbar />
